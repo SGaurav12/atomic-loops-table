a5366415aa793d901c269f81d47955a9dc00a0d2
SGaurav12 <gaurav.shende@atomicloops.com>
2025-05-20 10:30:01 +0530

    feat(auth): add logout functionality and conditional Header display in Layout

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,8 +1,29 @@
+import { useAuth } from "@/context/auth-context";
+import { Button } from "@/components/ui/button"; // if you're using shadcn's Button
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    logout();
+    navigate("/login");
+  }
+
   return (
     <header className="text-black pr-4 pb-4">
       <div className="max-w-8xl mx-auto px-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold">My Application</h1>
+
+        {user ? (
+          <div className="flex items-center gap-4">
+            <span className="text-sm text-gray-700">Hi, {user.email}</span>
+            <Button className="cursor-pointer" variant="outline" size="sm" onClick={handleClick}>
+              Logout
+            </Button>
+          </div>
+        ) : null}
       </div>
     </header>
   );
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,15 +1,17 @@
-import React, {type ReactNode} from 'react';
 import Header from './header';
 import { Outlet } from 'react-router-dom';
-
-
+import { useLocation } from 'react-router-dom';
 
 const Layout : React.FC = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+  const isRegisterPage = location.pathname === '/register';
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 container mx-auto p-4">
-        < Outlet/>
+    <div className="min-h-screen flex flex-col p-6">
+      {(!isLoginPage && !isRegisterPage) && <Header />}
+      <main className="relative flex-1 container mx-auto p-4 overflow-visible">
+        <Outlet/>
       </main>
     </div>
   );
